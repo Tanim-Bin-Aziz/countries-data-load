64053d0f80c5e9818b988343520ee8b47e878855
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-02-22 21:05:18 +0600

    countries data loaded

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
   return (
     <div>
-      <h3>Countires :</h3>
+      <h3>Countires : {countries.length}</h3>
     </div>
   );
 };
