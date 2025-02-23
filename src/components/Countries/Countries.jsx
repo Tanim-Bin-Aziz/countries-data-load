4935d178649e6c599ffb29c403873dbdc420f24d
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-02-23 11:25:11 +0600

    country data grid fixed

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -13,9 +14,11 @@ const Countries = () => {
   return (
     <div>
       <h3>Countires : {countries.length}</h3>
-      {countries.map((country) => (
-        <Country key={country.cca3} country={country}></Country>
-      ))}
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
