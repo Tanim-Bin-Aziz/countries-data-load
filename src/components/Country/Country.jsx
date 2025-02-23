b227cbe9368d9c2bab36548ac332c5dc1c2c9798
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-02-23 11:55:13 +0600

    conditional button fixed

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "./Country.css";
 const Country = ({ country }) => {
-  const { name, flags, population, area } = country;
+  const { name, flags, population, area, cca3 } = country;
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+
   return (
     <div className="country">
       <h3>Name : {name?.common}</h3>
       <img src={flags.png} alt="" />
       <p>population : {population}</p>
       <p>area : {area}</p>
+      <small>Code : {cca3}</small>
+      <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
+      {visited ? "I have visited" : " I want to visit"}
     </div>
   );
 };
