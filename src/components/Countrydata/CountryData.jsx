3dc835a1932335aa8847b9588e71f85ef82b4862
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-02-25 22:16:31 +0600

    some page not working issue

--- src/components/Countrydata/CountryData.jsx
+++ src/components/Countrydata/CountryData.jsx
@@ -1,9 +1,9 @@
-const CountryData = () => {
-  return (
-    <div>
-      <h3>this is Data</h3>
-    </div>
-  );
-};
+// const CountryData = () => {
+//   return (
+//     <div>
+//       <h3>this is Data</h3>
+//     </div>
+//   );
+// };
 
-export default CountryData;
+// export default CountryData;
